ccae94111b6fe4139b981fcd916aa29d307ba326
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2023-07-30 15:41:10 +0700

    Authentication minimalist

---
+++ lib/config/config.dart
@@ -0,0 +1,8 @@
+class Config {
+  static const String appName = "ITBCafeteria";
+  static const String apiURL = "https://itb-cafeteria-server.vercel.app";
+  // static const String apiURL = "http://192.168.238.17:3000";
+  static const String loginURL = apiURL + "/login";
+  static const String registerURL = apiURL + "/register";
+  static const String profileURL = apiURL + "/profile";
+}
